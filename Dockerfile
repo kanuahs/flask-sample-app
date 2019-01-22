3ab00dbebe6ad358e0d49219187ce6e498e20c92
kanuahs <kanuahs@gmail.com>
2019-01-22 12:57:11 +0530

    added all files

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:2-alpine
+
+COPY . /src
+
+RUN pip install -r /src/requirements.txt
+
+EXPOSE 5000
+
+ENTRYPOINT ["python", "/src/app.py"]
+
+#CMD ["python","/src/app.py"]
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    return "Hello from "+IPAddr+"!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
